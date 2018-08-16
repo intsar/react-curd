d0007789989d47b7441224246a76f9a799e64415
Intu <mohd.intsar@klinicapp.com>
2018-08-16 16:07:44 +0530

    code initialize

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+         <h1> Reach with Mohammad Intsar </h1><hr/>
+        </header>
+       
+
+
+
+
+
+
+
+
+
+
+
+       
+      </div>
+    );
+  }
+}
+
+export default App;
